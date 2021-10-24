2ba8ea092adad7162aacfa27ac034690b1bedd5f
Nooo37 <senpai.ackabar@gmail.com>
2021-10-24 14:55:25 +0200

    add aquarium-dark

---
+++ colors/aquarium-dark.lua
@@ -0,0 +1,23 @@
+-- Base16 aquarium dark color
+-- Author: based on http://github.com/FrenzyExists/aquarium-vim
+
+local M = {}
+
+M.base00 = "#20202a" -- ----
+M.base01 = "#2c2e3e" -- ---
+M.base02 = "#1a1a24" -- --
+M.base03 = "#313449" -- -
+M.base04 = "#cddbf9" -- +
+M.base05 = "#a7b7d6" -- ++
+M.base06 = "#3d2059" -- +++
+M.base07 = "#313449" -- ++++
+M.base08 = "#eac1c1" -- red
+M.base09 = "#e6dfb8" -- orange
+M.base0A = "#f6bbe7" -- yellow
+M.base0B = "#b1dba4" -- green
+M.base0C = "#e6dfb8" -- aqua/cyan
+M.base0D = "#ebb9b9" -- blue
+M.base0E = "#b8dceb" -- purple
+M.base0F = "#c8cca7" -- brown
+
+return M
