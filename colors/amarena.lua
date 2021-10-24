8d1c875e7469e19864b42c54845701384d96153a
Nooo37 <senpai.ackabar@gmail.com>
2021-10-24 14:56:04 +0200

    add amarena

---
+++ colors/amarena.lua
@@ -0,0 +1,23 @@
+-- base16 amarena color
+-- Author: based on amarena (https://github.com/elenapan/dotfiles)
+
+local M = {}
+
+M.base00 = "#1a2026"
+M.base01 = "#1e252c"
+M.base02 = "#4e5d6b"
+M.base03 = "#242d35"
+M.base04 = "#ffffff"
+M.base05 = "#ffffff"
+M.base06 = "#ffffff"
+M.base07 = "#2d3741"
+M.base08 = "#fb6396"
+M.base09 = "#f692b2"
+M.base0A = "#fb6396"
+M.base0B = "#94cf95"
+M.base0C = "#7fe4d2"
+M.base0D = "#6ec1d6"
+M.base0E = "#c269bc"
+M.base0F = "#c269bc"
+
+return M
