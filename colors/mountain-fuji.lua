55beb825a980d1615d2fe5f3673a90039db238df
Nooo37 <senpai.ackabar@gmail.com>
2021-10-29 21:43:47 +0200

    add: mountain fuji

---
+++ colors/mountain-fuji.lua
@@ -0,0 +1,23 @@
+-- base16 mountain fuji color
+-- Author: based on mountain fuji (https://github.com/mountain-theme)
+
+local M = {}
+
+M.base00 = "#0f0f0f"
+M.base01 = "#262626"
+M.base02 = "#393939"
+M.base03 = "#4c4c4c"
+M.base04 = "#cacaca"
+M.base05 = "#bfbfbf"
+M.base06 = "#a0a0a0"
+M.base07 = "#191919"
+M.base08 = "#c49ea0"
+M.base09 = "#ceb188"
+M.base0A = "#c4c19e"
+M.base0B = "#9ec49f"
+M.base0C = "#9ec3c4"
+M.base0D = "#a5b4cb"
+M.base0E = "#a39ec4"
+M.base0F = "#c49ec4"
+
+return M
