e4573d23ab45f7cd2f312c2b927829652ae277c3
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-02-14 16:56:33 -0800

    Committed

---
+++ colors/acario-dark.lua
@@ -0,0 +1,24 @@
+-- Acario Dark Doom Emacs Theme 
+-- Author:  gagbo <https://github.com/gagbo>
+-- Source: https://github.com/doomemacs/themes/blob/master/themes/doom-acario-dark-theme.el
+
+local M = {}
+
+M.base00 = "#0F1019" -- ----
+M.base01 = "#1e1e33" -- ---
+M.base02 = "#464A56" -- --
+M.base03 = "#585C6C" -- -
+M.base04 = "#767676" -- +
+M.base05 = "#959EA5"  -- ++
+M.base06 = "#B2B2B2"  -- +++
+M.base07 = "#CEDBE5" -- ++++
+M.base08 = "#d83441" -- red
+M.base09 = "#d8b941" -- orange
+M.base0A = "#79d836" -- green
+M.base0B = "#36d88d" -- aqua/cyan
+M.base0C = "#3679d8" -- blue
+M.base0D = "#8041d8" -- purple
+M.base0E = "#2d9574" -- teal
+M.base0F = "#AB11D8" -- violet 
+
+return M
---
+++ colors/vice.lua
@@ -0,0 +1,23 @@
+-- Base16 Vice (Alternative) color scheme 
+-- Author: Thomas Highbaugh 
+
+local M = {}
+
+M.base00 = "#181818" 
+M.base01 = "#222222" 
+M.base02 = "#323232" 
+M.base03 = "#3f3f3f" 
+M.base04 = "#666666" 
+M.base05 = "#818181" 
+M.base06 = "#c6c6c6" 
+M.base07 = "#e9e9e9" 
+M.base08 = "#ff29a8"
+M.base09 = "#85ffe0" 
+M.base0A = "#f0ffaa"
+M.base0B = "#0badff" 
+M.base0C = "#8265ff"
+M.base0D = "#00eaff" 
+M.base0E = "#00f6d9"
+M.base0F = "#ff3d81" 
+
+return M
